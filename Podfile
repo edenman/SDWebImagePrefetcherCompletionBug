be774a91a0f2002306a497c26f4c0abba687662d
Eric Denman <edenman@gmail.com>
2018-08-24 15:47:02 -0700

    fp

---
+++ Podfile
@@ -0,0 +1,12 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '8.1'
+use_frameworks!
+workspace 'SDWebImagePrefetcherCompletionBug'
+
+# ignore compiler warnings from Pods
+inhibit_all_warnings!
+
+target 'SDWebImagePrefetcherCompletionBug' do
+	pod 'SDWebImage'
+end
+
---
+++ SDWebImagePrefetcherCompletionBug/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SDWebImagePrefetcherCompletionBug
+//
+//  Created by Eric Denman on 8/24/18.
+//  Copyright © 2018 Coffee Train. All rights reserved.
+//
+
+import UIKit
+import SDWebImage
+
+class ViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    prefetch()
+  }
+
+  private func prefetch() {
+    let bogusURL = URL(string: "http://not_a_domain/lazy_bear.jpg")!
+    SDWebImagePrefetcher.shared().prefetchURLs([bogusURL], progress: nil, completed: { numFinished, numSkipped in
+      NSLog("OMG finished \(numFinished) skipped \(numSkipped)")
+      self.prefetch()
+    })
+  }
+}
